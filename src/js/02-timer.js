38815c2ae757a1f7a1664178353d0d1f0fa8f2ed
dashabondarenko07 <dashabondarenko0709@gmail.com>
2022-04-02 00:25:34 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,78 +1,137 @@
+// Описан в документации
 import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+// Notiflix Block Module import
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Additional import Notiflix Block Styles
+import 'notiflix/dist/notiflix-notify-aio-3.1.0.min.js';
 
-const startBtn = document.querySelector('[data-start]');
-const daysRef = document.querySelector('[data-days]');
-const hoursRef = document.querySelector('[data-hours]');
-const minutesRef = document.querySelector('[data-minutes]');
-const secondsRef = document.querySelector('[data-seconds]');
-let timerId = null;
+// global refs
+const refs = {
+  boxEl: document.createElement('div'),
+  pEl: document.querySelector('p'),
+  inputDateTimePicker: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+  divTimer: document.querySelector('.timer'),
+  selectedDate: 0,
+  dateNowGlobal: 0,
+  timerId: '',
+  daysValue: document.querySelector('span.value[data-days]'),
+  hoursValue: document.querySelector('span.value[data-hours]'),
+  minutesValue: document.querySelector('span.value[data-minutes]'),
+  secondsValue: document.querySelector('span.value[data-seconds]'),
+};
 
-startBtn.setAttribute('disabled', true);
+// added div.boxElement and disabled buttonStart
+refs.boxEl.className = 'boxElement';
+refs.boxEl.style.height = '40vh';
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+const btnReset = document.createElement('button');
+btnReset.className = 'resetBtn';
+btnReset.type = 'button';
+btnReset.setAttribute('data-reset', '');
+btnReset.textContent = 'Reset page';
+btnReset.style.backgroundColor = 'red';
+btnReset.style.color = 'black';
+btnReset.style.marginLeft = '40px';
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+refs.pEl.after(refs.boxEl);
+refs.boxEl.prepend(refs.inputDateTimePicker, refs.btnStart, btnReset);
+refs.boxEl.after(refs.divTimer);
+refs.btnStart.setAttribute('disabled', '');
 
-  return { days, hours, minutes, seconds };
-}
-
-const addLeadingZero = value => String(value).padStart(2, 0);
-
+// flatpickr options and init
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
-      Notify.failure('Please choose a date in the future');
-      return;
+    const dateNow = new Date();
+    refs.selectedDate = selectedDates[0].getTime();
+    console.log(selectedDates[0]);
+
+    if (selectedDates[0].getTime() >= dateNow.getTime()) {
+      refs.btnStart.removeAttribute('disabled');
+    } else {
+      refs.btnStart.setAttribute('disabled', '');
+      Notify.failure('Please choose a date in the future', {
+        clickToClose: true,
+        timeout: 4000,
+        position: 'center-center',
+        backOverlay: true,
+      });
+      //   window.alert("Please choose a date in the future");
     }
-    startBtn.removeAttribute('disabled');
-
-    const showTimer = () => {
-      const now = new Date();
-      localStorage.setItem('selectedData', selectedDates[0]);
-      const selectData = new Date(localStorage.getItem('selectedData'));
-
-      if (!selectData) return;
-
-      const diff = selectData - now;
-      const { days, hours, minutes, seconds } = convertMs(diff);
-      daysRef.textContent = days;
-      hoursRef.textContent = addLeadingZero(hours);
-      minutesRef.textContent = addLeadingZero(minutes);
-      secondsRef.textContent = addLeadingZero(seconds);
-
-      if (
-        daysRef.textContent === '0' &&
-        hoursRef.textContent === '00' &&
-        minutesRef.textContent === '00' &&
-        secondsRef.textContent === '00'
-      ) {
-        clearInterval(timerId);
-      }
-    };
-
-    const onClick = () => {
-      if (timerId) {
-        clearInterval(timerId);
-      }
-      showTimer();
-      timerId = setInterval(showTimer, 1000);
-    };
-
-    startBtn.addEventListener('click', onClick);
   },
 };
 
-flatpickr('#datetime-picker', { ...options });
+flatpickr(refs.inputDateTimePicker, options);
+
+// start timer
+refs.btnStart.addEventListener('click', () => {
+  refs.btnStart.setAttribute('disabled', '');
+  refs.inputDateTimePicker.setAttribute('disabled', '');
+
+  refs.timerId = setInterval(timerRun, 1000);
+});
+
+// reset page
+btnReset.addEventListener('click', () => {
+  document.location.reload();
+});
+
+function timerRun() {
+  refs.dateNowGlobal = new Date();
+  // console.log(refs.dateNowGlobal);
+
+  let deltaTimeMs = refs.selectedDate - refs.dateNowGlobal;
+
+  console.log(deltaTimeMs);
+  let deltaTimeMsObj = convertMs(deltaTimeMs);
+
+  // adding textContent to html elements days, hours, minutes, seconds
+  const { days, hours, minutes, seconds } = deltaTimeMsObj;
+
+  refs.daysValue.textContent = addLeadingZero(days);
+  refs.hoursValue.textContent = addLeadingZero(hours);
+  refs.minutesValue.textContent = addLeadingZero(minutes);
+  refs.secondsValue.textContent = addLeadingZero(seconds);
+
+  // check timer deltaTimeMs < 1s
+  if (deltaTimeMs < 1000) {
+    Notify.success('Timer is Over! Good luck!', {
+      clickToClose: true,
+      timeout: 4000,
+      position: 'center-center',
+    });
+    clearInterval(refs.timerId);
+  }
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  if (value < 10) {
+    return `${value}`.padStart(2, '0'); // "00000value"
+  } else {
+    return `${value}`;
+  }
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', createOnSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+function createOnSubmit(event) {
+  event.preventDefault();
+  if (!e.target.tagName === 'BUTTON') return;
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  delayInp = Number(delay.value);
+  stepInp = Number(step.value);
+  amountInp = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayInp)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayInp += stepInp;
+  };
+
+  e.currentTarget.reset();
+};
